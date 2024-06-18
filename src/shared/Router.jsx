0c561ca5cdc21fefc2adffdc331367eb4fc4622b
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-06-18 09:53:52 +0900

    프로젝트 기본 세팅 추가

---
+++ src/shared/Router.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from '../pages/MainPage/MainPage';
+import DetailPage from '../pages/DetailPage/DetailPage';
+import Layout from '../layouts/layout/Layout';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="detail/:id" element={<DetailPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
