0a99690647a0a97e1303b572b67b4672c80a7f04
reeny404 <happy2hyeon@kakao.com>
2024-06-18 14:19:06 +0900

    feat: fouter version 5로 사용되어 있어 불필요한 코드 제거

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import router from './router/router';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <App />
   </QueryClientProvider>
 );
