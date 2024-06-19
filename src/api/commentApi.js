5ca0ae78bc7c12ca0ae147d0a0740e4a8bf9b030
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-06-19 22:16:57 +0900

    comment 작성 기능 구현

---
+++ src/api/commentApi.js
@@ -0,0 +1,32 @@
+import supabase from '../supabase/supabaseClient';
+
+export const getComments = async () => {
+  try {
+    const { data, error } = await supabase.from('comments').select('*');
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    console.error('Failed to get comment:', error.message);
+    throw new Error(`Failed to get comment : ${error.message}`);
+  }
+};
+
+export const addComments = async (comment) => {
+  try {
+    const { body, created_at, id } = comment;
+    if (!body) {
+      throw new Error('Body text are required');
+    }
+    const { data, error } = await supabase
+      .from('comments')
+      .insert([{ body: body, id: id, created_at: created_at }]);
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (error) {
+    throw new Error(`Failed to add comment: ${error.message}`);
+  }
+};
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,88 @@
-import React from 'react';
+import {
+  useQuery,
+  QueryClient,
+  useQueryClient,
+  useMutation,
+} from '@tanstack/react-query';
+import React, { useRef, useState } from 'react';
+import { addComments, getComments } from '../api/commentApi';
+import uuid4 from 'uuid4';
 
 const Comment = () => {
+  const commentRef = useRef(null);
+  const queryClient = useQueryClient();
+
+  const {
+    data: comments,
+    isPending,
+    isSuccess,
+  } = useQuery({
+    queryKey: ['comments'],
+    queryFn: getComments,
+  });
+
+  const mutationAdd = useMutation({
+    mutationFn: addComments,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['comments']);
+    },
+  });
+
+  const handleSubmitComment = (event) => {
+    event.preventDefault();
+    const commentBody = commentRef.current.value;
+    const newComment = {
+      id: uuid4(),
+      body: commentBody,
+      created_at: new Date().toISOString(),
+    };
+
+    if (isSuccess) {
+      console.log('newComment => ', newComment);
+      mutationAdd.mutate(newComment);
+      commentRef.current.value = '';
+    }
+  };
+
+  if (isSuccess) {
+    console.log('comments => ', comments);
+  }
+
   return (
-    <div className="bg-darkgray w-full h-full rounded-2xl p-7">
-      <p>Comments</p>
-      <div>Comment Box</div>
+    <div className="flex flex-col bg-darkgray w-full h-[50%] rounded-2xl p-9 gap-3">
+      <p className="text-xl">한 줄 응원하기</p>
+      <form
+        className="flex gap-3 w-full h-[10%] justify-between items-center gap-1 text-darkgray"
+        onSubmit={handleSubmitComment}
+      >
+        <input
+          type="text"
+          placeholder="우리팀 화이팅!!!"
+          className="p-2 rounded-md w-[100%] h-full focus:outline-none"
+          ref={commentRef}
+        />
+        <button
+          type="submit"
+          className="rounded-md bg-white px-4 py-2 min-w-[110px] h-full inline-block hover:bg-[#3D69CB] hover:text-white"
+        >
+          작성하기
+        </button>
+      </form>
+      <ul className="grid auto-rows-[10%] gap-1 p-3 bg-white text-darkgray h-[100%] rounded-md overflow-y-auto">
+        {isPending && <div>댓글이 로딩중입니다...</div>}
+        {isSuccess &&
+          comments.map((comment, index) => {
+            const date = comment.created_at.split('.', 1)[0].split('T', 2)[0];
+            const time = comment.created_at.split('.', 1)[0].split('T', 2)[1];
+            // console.log(date, time);
+            return (
+              <li className="flex justify-between p-2" key={index}>
+                <span>{comment.body}</span>
+                <span>{date + ' ' + time}</span>
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 };
