3d2ddafc97ab85e3d431ac289e32c1656934a589
dongkyusq <hasd789@naver.com>
2024-06-20 12:49:57 +0900

    ds

--- src/api/youtubeApi.js
+++ src/api/youtubeApi.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-const youtubeKey = import.meta.env.VITE_YOUTUBE_KEY;
-console.log('youtubeKey', youtubeKey);
-
 class YoutubeAPI {
   #client;
 
@@ -12,55 +9,8 @@ class YoutubeAPI {
     });
   }
 
-  // 검색 기능 구현
   async search(keyword) {
-    const response = await this.#client.get('/search', {
-      params: {
-        part: 'snippet',
-        q: keyword,
-        maxResults: 12,
-        type: 'video',
-        key: youtubeKey,
-      },
-      headers: {
-        Accept: 'application/json',
-      },
-    });
-    const data = response.data;
-    return data.items;
-  }
-
-  // 채널에서 재생목록
-  async fetchPlaylistItems(playlistId) {
-    const response = await this.#client.get('/playlistItems', {
-      params: {
-        part: 'snippet,contentDetails',
-        playlistId: playlistId,
-        maxResults: 50,
-        key: youtubeKey,
-      },
-      headers: {
-        Accept: 'application/json',
-      },
-    });
-    const data = response.data;
-    return data;
-  }
-
-  // 비디오 id
-  async fetchVideoDetails(videoIds) {
-    const response = await this.#client.get('/videos', {
-      params: {
-        part: 'statistics',
-        id: videoIds.join(','),
-        key: youtubeKey,
-      },
-      headers: {
-        Accept: 'application/json',
-      },
-    });
-    const data = response.data;
-    return data.items;
+    return [];
   }
 }
 
