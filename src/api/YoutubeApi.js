136d0f736861e32d270b5580b79aea7f0df13b9d
reeny404 <happy2hyeon@kakao.com>
2024-06-20 12:37:14 +0900

    feat: YoutubeApi 충돌

--- src/api/YoutubeApi.js
+++ src/api/YoutubeApi.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const youtubeKey = import.meta.env.VITE_YOUTUBE_KEY;
+
 class YoutubeAPI {
   #client;
 
@@ -9,8 +11,55 @@ class YoutubeAPI {
     });
   }
 
+  // 검색 기능 구현
   async search(keyword) {
-    return [];
+    const response = await this.#client.get('/search', {
+      params: {
+        part: 'snippet',
+        q: keyword,
+        maxResults: 12,
+        type: 'video',
+        key: youtubeKey,
+      },
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const data = response.data;
+    return data.items;
+  }
+
+  // 채널에서 재생목록
+  async fetchPlaylistItems(playlistId) {
+    const response = await this.#client.get('/playlistItems', {
+      params: {
+        part: 'snippet,contentDetails',
+        playlistId: playlistId,
+        maxResults: 50,
+        key: youtubeKey,
+      },
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const data = response.data;
+    return data;
+  }
+
+  // 비디오 id
+  async fetchVideoDetails(videoIds) {
+    const response = await this.#client.get('/videos', {
+      params: {
+        part: 'statistics',
+        id: videoIds.join(','),
+        key: youtubeKey,
+      },
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const data = response.data;
+    return data.items;
   }
 }
 
